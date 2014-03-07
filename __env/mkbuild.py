d402d09bb33f5dbda63c4109f51fb32af2deffba
Andrew Savonichev <savonichev.andr@gmail.com>
2014-03-08 00:59:46 +0400

    mkbuild.py functionality added

--- __env/mkbuild.py
+++ __env/mkbuild.py
@@ -2,7 +2,10 @@
 
 from md_env import MD_ROOT
 import os
+import shutil
 import argparse
+import git
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--hash', dest="build_hash", required=True, help='Hash of git commit')
@@ -16,7 +19,20 @@ builds_dir = os.path.join( MD_ROOT, "builds", args.build_hash )
 if not os.path.isdir( builds_dir ):
     os.makedirs( builds_dir )
 else:
-    os.rmdir( builds_dir )
+    shutil.rmtree( builds_dir )
     os.makedirs( builds_dir )
 
+tmp_dir = os.path.join( MD_ROOT, "tmp" )
 
+if not os.path.isdir( tmp_dir ):
+    os.makedirs( tmp_dir )
+
+repo = git.Repo( MD_ROOT )
+head = repo.head.commit.sha
+repo.git.checkout( args.build_hash )
+
+os.chdir( tmp_dir )
+
+subprocess.call( [ "cmake" , MD_ROOT ])
+
+repo.git.checkout( head )
