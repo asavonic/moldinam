2489b544e1d5e94c1abe5a28afb164a22c4ae0a5
Andrew Savonichev <savonichev.andr@gmail.com>
2014-03-08 02:14:04 +0400

    mkbuild.py cmake options updated

--- __env/mkbuild.py
+++ __env/mkbuild.py
@@ -9,18 +9,21 @@ import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--hash', dest="build_hash", required=True, help='Hash of git commit')
+parser.add_argument('--debug', dest="build_type", action='store_const', const="Debug", default="Release", 
+                        help='Build debug binaries')
+
 args = parser.parse_args()
 
 
 args = parser.parse_args()
 
-builds_dir = os.path.join( MD_ROOT, "builds", args.build_hash )
+build_dir = os.path.join( MD_ROOT, "builds", args.build_hash )
 
-if not os.path.isdir( builds_dir ):
-    os.makedirs( builds_dir )
+if not os.path.isdir( build_dir ):
+    os.makedirs( build_dir )
 else:
-    shutil.rmtree( builds_dir )
-    os.makedirs( builds_dir )
+    shutil.rmtree( build_dir )
+    os.makedirs( build_dir )
 
 tmp_dir = os.path.join( MD_ROOT, "tmp" )
 
@@ -33,6 +36,9 @@ repo.git.checkout( args.build_hash )
 
 os.chdir( tmp_dir )
 
-subprocess.call( [ "cmake" , MD_ROOT ])
+subprocess.call( [ "cmake" , MD_ROOT, "-DCMAKE_INSTALL_PREFIX=" + build_dir,
+                   "-DCMAKE_BUILD_TYPE=" + args.build_type, "-DCMAKE_CXX_FLAGS=" + "-O2" ])
+
+subprocess.call( [ "make", "install"])
 
 repo.git.checkout( head )
