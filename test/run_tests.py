79d0303d90423658ddf4942218e6b87ab577fd37
Andrew Savonichev <savonichev.andr@gmail.com>
2014-03-09 15:14:22 +0400

    run_tests.py added

---
+++ test/run_tests.py
@@ -0,0 +1,18 @@
+import unittest
+import subprocess
+import os
+import shutil
+
+class MDTestCase(unittest.TestCase):
+    def setUp( self ) :
+        pass
+    def tearDown( self ) :
+        pass
+    def test_test1( self ):
+        test_exe = os.path.join( os.getcwd(), "cpp_sorter_test" )
+        self.assertEqual(subprocess.call( [ test_exe ] ), 0 )
+
+suite = unittest.TestLoader().loadTestsFromTestCase(MDTestCase)
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite)
