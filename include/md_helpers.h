07c6cee994453835dc9714b806967067778a3cbb
Andrew Savonichev <savonichev.andr@gmail.com>
2014-04-10 01:21:45 +0400

    Check for successful open of file added to helpers

--- include/md_helpers.h
+++ include/md_helpers.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <string>
 #include <md_types.h>
+#include <stdexcept>
 
 std::vector<Molecule> read_molecules_from_file( std::string filepath );
 void write_molecules_to_file( std::vector<Molecule>& molecules, std::string filepath, std::ios::openmode mode = std::ios::trunc );
--- lib/md_helpers.cpp
+++ lib/md_helpers.cpp
@@ -3,6 +3,10 @@
 std::vector<Molecule> read_molecules_from_file( std::string filepath ) {
     std::ifstream file( filepath.c_str(), std::ifstream::in );
 
+    if ( !file.is_open() ) {
+        throw std::runtime_error( "Error while opening file " + filepath + " for reading! Check if it exist and have correct permissions." );
+    }
+
     std::string line;
 
     int lines_num = 0;
@@ -22,6 +26,10 @@ std::vector<Molecule> read_molecules_from_file( std::string filepath ) {
 
 void write_molecules_to_file( std::vector<Molecule>& molecules, std::string filepath, std::ios::openmode mode ) {
     std::ofstream file( filepath.c_str(), std::ifstream::out | mode );
+
+    if ( !file.is_open() ) {
+        throw std::runtime_error( "Error while opening file " + filepath + " for writing! Check if it exist and have correct permissions." );
+    }
 
     file << molecules.size() << std::endl;
 
