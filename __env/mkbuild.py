adb4d278330a24ce70bb0bb3d22931f0c19c8c57
Andrew Savonichev <savonichev.andr@gmail.com>
2014-05-04 20:49:45 +0400

    mkbuild.py without --hash now builds current state

--- __env/mkbuild.py
+++ __env/mkbuild.py
@@ -8,14 +8,14 @@ import git
 import subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--hash', dest="build_hash", required=True, help='Hash of git commit')
+parser.add_argument('--hash', dest="build_hash", help='Hash of git commit')
 parser.add_argument('--debug', dest="build_type", action='store_const', const="Debug", default="Release", 
                         help='Build debug binaries')
 
 args = parser.parse_args()
 
-
-args = parser.parse_args()
+if not args.build_hash: 
+    args.build_hash = "current"
 
 build_dir = os.path.join( MD_ROOT, "builds", args.build_hash )
 
@@ -30,11 +30,11 @@ tmp_dir = os.path.join( MD_ROOT, "tmp" )
 if not os.path.isdir( tmp_dir ):
     os.makedirs( tmp_dir )
 
-repo = git.Repo( MD_ROOT )
+if args.build_hash != "current":
+    repo = git.Repo( MD_ROOT )
+    active_branch = repo.active_branch
+    repo.git.checkout( args.build_hash )
 
-active_branch = repo.active_branch
-
-repo.git.checkout( args.build_hash )
 
 os.chdir( tmp_dir )
 
@@ -43,4 +43,5 @@ subprocess.call( [ "cmake" , MD_ROOT, "-DCMAKE_INSTALL_PREFIX:PATH=" + build_dir
 
 subprocess.call( [ "make", "install" ])
 
-repo.git.checkout( active_branch )
+if args.build_hash != "current":
+    repo.git.checkout( active_branch )
