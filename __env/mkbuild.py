eac4ac3cbd95a2850c9d178cf12bf426fdebc878
Andrew Savonichev <savonichev.andr@gmail.com>
2014-09-11 03:43:02 +0400

    mkbuild.py changed to use python2

--- __env/mkbuild.py
+++ __env/mkbuild.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/bin/env python2
 
 from md_env import MD_ROOT
 import os
@@ -12,16 +12,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--hash', dest="build_hash", help='Hash of git commit')
 parser.add_argument('--debug', dest="build_type", action='store_const', const="Debug", default="Release", 
                         help='Build debug binaries')
-parser.add_argument('--compiler', dest="compiler", help='available options are gcc, intel')
+parser.add_argument('--compiler', dest="compiler", default="gcc", help='available options are gcc, intel')
 
 
 args = parser.parse_args()
 
 if args.compiler not in [ "gcc", "intel" ]:
-    print "Error! Unsupported value for --compiler"
+    print( "Error! Unsupported value for --compiler: " + args.compiler )
 
 compiler_opt = ""
 if args.compiler == "gcc":
+    print( "Using compiler: " + args.compiler.upper() )
     compiler_opt = "-DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++"
 if args.compiler == "intel":
     compiler_opt = "-DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icc"
@@ -54,7 +55,7 @@ subprocess.call( [ "cmake" , MD_ROOT, "-DCMAKE_INSTALL_PREFIX:PATH=" + build_dir
                    "-DCMAKE_BUILD_TYPE=" + args.build_type, "-DCMAKE_CXX_FLAGS=" + "-O2 " + compiler_opt ])
 
 if platform.system() == "Windows":
-    print "Now you can open tmp/moldynam.sln in Visual Studio and build whatever you want"
+    print( "Now you can open tmp/moldynam.sln in Visual Studio and build whatever you want" )
 else:
     subprocess.call( [ "make", "install" ])
 
