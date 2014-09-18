dc36aad87fa7af9cefb4f22256295e470c7f9da6
Andrew Savonichev <savonichev.andr@gmail.com>
2014-09-18 15:16:35 +0400

    Add mouse callback to glfw_wrapper

--- visual/glfw_wrapper/glfw_wrapper.hpp
+++ visual/glfw_wrapper/glfw_wrapper.hpp
@@ -64,6 +64,17 @@ namespace glfw {
      */
     static void g_resize_callback( GLFWwindow* glfw_window_ptr, int width, int height );
 
+    /**
+     * @brief Internal callback for mouse events. Finds window object according to glfw_window_ptr and calls its mouse_callback() method
+     *
+     * @param glfw_window_ptr is a pointer to plain GLFW window
+     * @param key is the keyboard key that was pressed or released. 
+     * @param scancode The system-specific scancode of the key. 
+     * @param action GLFW_PRESS, GLFW_RELEASE or GLFW_REPEAT ( when mouse button held down, repeats on release ). 
+     * @param mods Bit field describing which modifier keys were held down. GLFW_MOD_SHIFT, GLFW_MOD_CONTROL, GLFW_MOD_ALT, GLFW_MOD_SUPER
+     */
+    static void g_mouse_callback( GLFWwindow* glfw_window_ptr, int key, int scancode, int action, int mods );
+
 
     /**
      * @brief Class represents window object and contains methods to create and manipulate windows
@@ -153,6 +164,18 @@ namespace glfw {
             _height = height;
         }
 
+        /**
+         * @brief Called when some mouse evet occured. Does nothing in glfw::window, but can be overloaded in derived class
+         *
+         * @param key Mouse button key: GLFW_MOUSE_BUTTON_LEFT, GLFW_MOUSE_BUTTON_RIGHT, GLFW_MOUSE_BUTTON_MIDDLE
+         * @param scancode The system-specific scancode of the key. 
+         * @param action GLFW_PRESS, GLFW_RELEASE or GLFW_REPEAT. 
+         * @param mods Bit field describing which modifier keys were held down. GLFW_MOD_SHIFT, GLFW_MOD_CONTROL, GLFW_MOD_ALT, GLFW_MOD_SUPER
+         */
+        virtual void mouse_callback( int key, int scancode, int action, int mods ) {
+
+        }
+
         private:
         GLFWwindow* _window;
 
@@ -161,13 +184,30 @@ namespace glfw {
     };
 
 
-    static void g_resize_callback( GLFWwindow* glfw_window_ptr, int width, int height ) {
+    /**
+     * @brief Finds glfw::window object according to glfw_window_ptr. Throw runtime_error if not found ( exceptional case and it is most likely an internal bug )
+     *
+     * @param glfw_window_ptr Plain GLFWwindow pointer to be compared with glfw::window:_window
+     *
+     * @return glfw::window object for plain GLFWwindow
+     */
+    static window* g_find_window_object( GLFWwindow* glfw_window_ptr ) {
         auto iter = std::find_if( g_window_object_list.begin(), g_window_object_list.end(), [&]( std::pair< window* , GLFWwindow* > item ) { return &(*glfw_window_ptr) == &(*item.second ); } );
-        if ( iter != g_window_object_list.end() ) { 
-            window* win_ptr = iter->first;
-            win_ptr->resize_callback( width, height );
-        } else {
-            throw std::runtime_error( "Resize callback was unable to find window object" );
+
+        if ( iter == g_window_object_list.end() ) { 
+            throw std::runtime_error( "Callback was unable to find window object" );
         }
+
+        return iter->first;
+    }
+
+    static void g_resize_callback( GLFWwindow* glfw_window_ptr, int width, int height ) {
+        window* win_ptr = g_find_window_object( glfw_window_ptr );
+        win_ptr->resize_callback( width, height );
+    }
+
+    static void g_mouse_callback( GLFWwindow* glfw_window_ptr, int key, int scancode, int action, int mods ) {
+        window* win_ptr = g_find_window_object( glfw_window_ptr );
+        win_ptr->mouse_callback( key, scancode, action, mods );
     }
 }
