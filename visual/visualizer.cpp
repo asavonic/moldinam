34664737695200e0922b5723201933498167d2ea
Andrew Savonichev <savonichev.andr@gmail.com>
2014-09-21 22:09:20 +0400

    draw a rectangle with shaders

--- visual/visualizer.cpp
+++ visual/visualizer.cpp
@@ -2,11 +2,14 @@
 #include "glfw_wrapper/glfw_wrapper.hpp"
 #include "glm/glm.hpp"
 #include <glm/gtc/type_ptr.hpp>
+#include <iostream>
 
 class cube_window : public glfw::window {
 
     public:
     cube_window() : glfw::window() {
+        init_program();
+        init_VBO();
     }
     void init_program() {
         const GLchar* vsSource = 
@@ -19,7 +22,7 @@ class cube_window : public glfw::window {
 
         const GLchar* fsSource = 
         "void main() {\n"
-        "  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n"
+        "  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
         "}\n";
 
         
@@ -29,39 +32,67 @@ class cube_window : public glfw::window {
         glShaderSource(vShader, 1, &vsSource, NULL);
         glCompileShader(vShader);
 
+        shaderLog( vShader );
 
         fShader = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fShader, 1, &fsSource, NULL);
         glCompileShader(fShader);
-        GLuint program = glCreateProgram();
+
+        shaderLog( fShader );
+        program = glCreateProgram();
         glAttachShader(program, vShader);
         glAttachShader(program, fShader);
         glLinkProgram(program);
 
+        checkOpenGLerror();
         unif_mvp = glGetUniformLocation( program, "mvp" );
         attrib_vertex = glGetAttribLocation( program, "coord" );
     }
 
+void shaderLog(unsigned int shader) 
+{ 
+  int   infologLen   = 0;
+  int   charsWritten = 0;
+  char *infoLog;
+
+  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infologLen);
+
+  if(infologLen > 1)
+  { 
+    infoLog = new char[infologLen];
+    if(infoLog == NULL)
+    {
+      std::cout<<"ERROR: Could not allocate InfoLog buffer\n";
+       exit(1);
+    }
+    glGetShaderInfoLog(shader, infologLen, &charsWritten, infoLog);
+    std::cout<< "InfoLog: " << infoLog << "\n\n\n";
+    delete[] infoLog;
+  }
+}
+
+void checkOpenGLerror()
+{
+  GLenum errCode;
+  if((errCode=glGetError()) != GL_NO_ERROR)
+    std::cout << "OpenGl error! - " << gluErrorString(errCode) << std::endl;
+}
     void init_VBO() {
         glGenBuffers(1, &VBO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         
-        std::vector< glm::vec3 > cube;
-		cube.push_back( glm::vec3( -0.5, -0.5, -0.5 ) );
-        cube.push_back( glm::vec3( 0.5, -0.5, -0.5 ) );
-        cube.push_back( glm::vec3( 0.5, -0.5, 0.5 ) );
-        cube.push_back( glm::vec3( -0.5, -0.5, 0.5 ) );
-        cube.push_back( glm::vec3( 0.5, 0.5, -0.5 ) );
-        cube.push_back( glm::vec3( 0.5, 0.5, 0.5 ) );
-        cube.push_back( glm::vec3( -0.5, 0.5, 0.5 ) );
-        cube.push_back( glm::vec3( -0.5, 0.5, -0.5 ) ); 
+		cube.push_back( glm::vec3( -0.5, -0.5, 0 ) );
+        cube.push_back( glm::vec3( 0.5, -0.5, 0 ) );
+        cube.push_back( glm::vec3( 0.5, 0.5, 0 ) );
+        cube.push_back( glm::vec3( -0.5, 0.5, 0 ) );
 
-        glBufferData(GL_ARRAY_BUFFER, cube.size() * sizeof( glm::vec3 ), glm::value_ptr( cube ), GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, cube.size() * sizeof( glm::vec3 ), glm::value_ptr( cube[0] ), GL_STATIC_DRAW);
     }
 
 
 
     virtual void draw() {
+        glClearColor(0, 0, 0, 0);
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(program); 
         glm::mat4 mvp( 1.0f );
@@ -70,12 +101,14 @@ class cube_window : public glfw::window {
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glVertexAttribPointer(attrib_vertex, 3, GL_FLOAT, GL_FALSE, 0, 0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glDrawArrays( GL_LINE_LOOP, 0, 8 );
+        glDrawArrays( GL_LINE_LOOP, 0, cube.size() );
+        checkOpenGLerror();
     }
 
     GLuint program;
     GLuint attrib_vertex;
     GLuint unif_mvp;
+    std::vector< glm::vec3 > cube;
 
     GLuint VBO;
 };
