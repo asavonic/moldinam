702be8261abffe9a1470aac31d0476446f22cead
Andrew Savonichev <andrew.savonichev@gmail.com>
2015-04-08 01:52:56 +0300

    using glm vector types and funcs for native platform

---
+++ src/include/platforms/native/types.hpp
@@ -0,0 +1,10 @@
+#include <glm/glm.hpp>
+#include <vector>
+
+namespace md {
+    typedef glm::vec3 float3;
+    typedef std::vector<float3> float3vec;
+
+    using glm::floor;
+    using glm::distance;
+}
