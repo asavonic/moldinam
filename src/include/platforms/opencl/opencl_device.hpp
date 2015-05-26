513b15934fba0462c9e1c49614a9510a911b2bd7
Andrew Savonichev <andrew.savonichev@gmail.com>
2015-05-26 18:25:31 +0300

    OpenCLDevice::CreateProgram can use multiple sources

--- src/include/platforms/opencl/opencl_device.hpp
+++ src/include/platforms/opencl/opencl_device.hpp
@@ -17,9 +17,10 @@ public:
 
     cl::CommandQueue& get_queue() { return m_queue; }
 
-    cl::Program CreateProgram(cl::Program::Sources source, const char* build_options = NULL)
+    cl::Program CreateProgram(std::string source, const char* build_options = NULL)
     {
-        cl::Program program(m_context, source);
+        cl::Program::Sources src(1, std::make_pair(source.c_str(), source.length()));
+        cl::Program program(m_context, src);
         try {
             program.build(build_options);
         } catch (cl::Error& err) {
@@ -29,6 +30,47 @@ public:
         }
 
         return program;
+    }
+
+    cl::Program CreateProgram(std::vector<std::string> sources, const char* build_options = NULL)
+    {
+        std::vector<cl::Program> programs;
+        cl::Program linked_program;
+
+        try {
+            for (std::string source : sources) {
+                cl::Program::Sources src(1, std::make_pair(source.c_str(), source.length()));
+                programs.emplace_back(m_context, src);
+                programs.back().compile(build_options);
+            }
+        } catch (cl::Error& err) {
+            std::cout << "program build failed:\n";
+            for (auto& prog : programs) {
+                std::cout << prog.getBuildInfo<CL_PROGRAM_BUILD_LOG>(m_devices[0]) << std::endl;
+            }
+            std::cout << std::endl;
+            throw;
+        }
+
+        // NOTE: cl.hpp have linkPrograms() functions
+        // but for some reason it does not allows to specifiy context and uses default one
+        // this leads to link failure and some other funny stuff.
+
+        std::vector<cl_program> compiled_progs(programs.size(), 0);
+        for (size_t i = 0; i < compiled_progs.size(); ++i) {
+            compiled_progs[i] = programs[i]();
+        }
+
+        cl_int error = CL_SUCCESS;
+
+        linked_program = ::clLinkProgram(m_context(), 0, NULL, NULL, compiled_progs.size(),
+                                         &compiled_progs[0], NULL, NULL, &error);
+
+        if (error != CL_SUCCESS) {
+            throw cl::Error(error, "clLinkProgram");
+        }
+
+        return linked_program;
     }
 
 
