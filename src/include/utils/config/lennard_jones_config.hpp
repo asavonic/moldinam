c9c749bc738910d7b18335be39dae7d501be5197
Andrew Savonichev <andrew.savonichev@gmail.com>
2015-05-31 22:20:33 +0300

    add lj constants specialization for floats

--- src/include/utils/config/lennard_jones_config.hpp
+++ src/include/utils/config/lennard_jones_config.hpp
@@ -8,20 +8,21 @@ class LennardJonesConstants
 
     LennardJonesConstants() {
         sigma = sigma_pow_2 = sigma_pow_6 = sigma_pow_12 = 1;
+        sigmaf = sigmaf_pow_2 = sigmaf_pow_6 = sigmaf_pow_12 = 1;
         eps = eps_pow_6 = eps_pow_12 = 1;
     }
 
     void set_sigma( double _sigma ) {
-        sigma = _sigma;
-        sigma_pow_2 = std::pow( sigma, 2 );
-        sigma_pow_6 = std::pow( sigma, 6 );
-        sigma_pow_12 = std::pow( sigma, 12 );
+        sigmaf = sigma = _sigma;
+        sigmaf_pow_2 = sigma_pow_2 = std::pow( sigma, 2 );
+        sigmaf_pow_6 = sigma_pow_6 = std::pow( sigma, 6 );
+        sigmaf_pow_12 = sigma_pow_12 = std::pow( sigma, 12 );
     }
 
     void set_eps( double _eps ) {
-        eps = _eps;
-        eps_pow_6 = std::pow( eps, 6 );
-        eps_pow_12 = std::pow( eps, 12 );
+        epsf = eps = _eps;
+        epsf_pow_6 = eps_pow_6 = std::pow( eps, 6 );
+        epsf_pow_12 = eps_pow_12 = std::pow( eps, 12 );
     }
 
     template<typename T = double>
@@ -43,6 +44,9 @@ private:
     double sigma;
     double eps;
 
+    float sigmaf;
+    float epsf;
+
     // precomputed pow( sigma, N ) used due to performance reasons
     double sigma_pow_2;
 
@@ -51,7 +55,28 @@ private:
 
     double sigma_pow_12;
     double eps_pow_12;
+
+    float sigmaf_pow_2;
+
+    float sigmaf_pow_6;
+    float epsf_pow_6;
+
+    float sigmaf_pow_12;
+    float epsf_pow_12;
 };
+
+template<>
+inline float LennardJonesConstants::get_sigma<float>() const { return sigmaf; }
+template<>
+inline float LennardJonesConstants::get_sigma_pow_2<float>() const { return sigmaf_pow_2; }
+template<>
+inline float LennardJonesConstants::get_sigma_pow_6<float>() const { return sigmaf_pow_6; }
+template<>
+inline float LennardJonesConstants::get_sigma_pow_12<float>() const { return sigmaf_pow_12; }
+
+template<>
+inline float LennardJonesConstants::get_eps<float>() const { return epsf; }
+
 
 // TODO: implement constants for different types
 class LennardJonesConfig : public IConfig {
